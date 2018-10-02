aa1b60a8d4636b771aeb9fd53fc02fc24f24b580
Kyuu0707 <qlosborn@yahoo.com>
2018-10-02 11:12:32 -0700

    click tag list button from illust page

--- LegacyPixivTags.user.js
+++ LegacyPixivTags.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Pixiv legacy tag list
 // @namespace    https://github.com/Kyuu0707/
-// @version      1.0
+// @version      1.1
 // @description  adds button to send user to legacy tag list
 // @author       Kyuu
 // @match        https://www.pixiv.net/member*
@@ -11,6 +11,10 @@
 function goToTags()
 {
     var url = window.location.href;
+    if(url = "*pixiv.net/member_ill*")
+    {
+        url = document.getElementsByClassName("e165rlrk3")[0].getAttribute("href");
+    }
     var idx = url.indexOf('=');
     url = url.slice(idx+1,url.length);
     //console.log(url);
