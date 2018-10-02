dd87a2f708b9b53db83b04ef415ae220ff5d94e9
Kyuu0707 <qlosborn@yahoo.com>
2018-10-02 00:40:31 -0700

    Create LegacyPixivTags.js

---
+++ LegacyPixivTags.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name         Pixiv legacy tag list
+// @namespace    lmao who knows man
+// @version      0.9
+// @description  adds button to send user to legacy bookmarks list
+// @author       Kyuu
+// @match        https://www.pixiv.net/member*
+// @grant        none
+// ==/UserScript==
+
+function goToTags()
+{
+    var url = window.location.href;
+    var idx = url.indexOf('=');
+    url = url.slice(idx+1,url.length);
+    //console.log(url);
+    window.location.href = "https://www.pixiv.net/member_tag_all.php?id=" + url;
+}
+
+function main()
+{
+    'use strict';
+
+    var elems = document.getElementsByClassName('n5QTR4w');
+    var myNode = elems[0];
+    console.log(myNode);
+    var myButton = document.createElement("Button");
+    myButton.innerHTML = "Bookmarks";
+    myButton.addEventListener('click',goToTags);
+    myNode.appendChild(myButton);
+}
+
+window.onload = function()
+{
+    main();
+}
